6159687313fdcb79b760e9f83cdf86d8006e3d9e
abdulku1234 <abdulkuddus@gmail.com>
2023-06-22 00:20:51 -0700

    efe

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+ let myFunctionE = a => a*a; 
+    
+ 
+
+    //call//
+   console.log(myFunctionE(4));
+
+
+  //let students={
+  //  fname:"Aadil",
+    //lname:'shah',
+   // mobileno:'755454897645',
+   // address:'neemuch'
+ // }
+ // let { fname } = students;
+ // console.log(fname)
+
+ 
